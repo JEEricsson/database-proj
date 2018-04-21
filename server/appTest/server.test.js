fed0f42dee4d113b8bd6d667eb58c53e1c50ab9c
JE Ericsson <Crypt055@Macintosh.local>
2018-04-21 19:49:27 +0200

    Creating a test for POST purchases

---
+++ server/appTest/server.test.js
@@ -0,0 +1,55 @@
+const expect = require('expect')
+const request = require('supertest')
+
+const {app} = require('./../server')
+const {Purchase} = require('./../models/purchase')
+
+beforeEach((done) => {
+    Purchase.remove({}).then(() => done())
+})
+
+describe('POST /purchases', () => {
+    it('creating a new purchase', (done) => {
+        let text = 'Inserting test'
+        let clientId = '3457dfwsw3'
+        let price = 34
+        
+
+        request(app)
+        .post('/purchases')
+        .send({text, clientId, price})
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.text).toBe(text)
+            expect(res.body.clientId).toBe(clientId)
+            expect(res.body.price).toBe(price)
+        })
+        .end((err, res) => {
+            if (err){
+                return done(err)
+            }
+
+            Purchase.find().then((purchases) => {
+                expect(purchases.length).toBe(1)
+                expect(purchases[0].text).toBe(text)
+                done()
+            }).catch((e) => done(e))
+        })
+    })
+
+    it('Should not create a purchase that is not valid', (done) => {
+        request(app)
+        .post('/purchases')
+        .send({})
+        .expect(400)
+        .end((err) => {
+        if (err){
+            return done(err)
+        }
+    })
+        Purchase.find().then((purchases) => {
+            expect(purchases.length).toBe(0)
+            done()
+        }).catch((e) => done(e))
+    })   
+})
--- server/server.js
+++ server/server.js
@@ -42,3 +42,5 @@ app.post('/users', (req, res) => { //recieving a http POST request to create new
 app.listen(3000, () => {
     console.log('Server started on port 3000')
 })
+
+module.exports = {app}
